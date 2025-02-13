54bab30c44cf7dfac6bd95179b92384f52080c1d
Amanda <amanda.rosado3@upr.edu>
2025-02-13 17:03:51 +0000

    commit1.1

--- main.cpp
+++ main.cpp
@@ -7,9 +7,35 @@ using namespace std;
 
 
 int main(){
-  
+
   cout << "Este programa suma dos fracciones elegidas por el usuario." << endl;
 
+
+  cout << "Entre las fracciones: " << endl;
+
+  int numerador1;
+  cout << "Entre el numerador de su primera fraccion: " << endl;
+  cin >> numerador1;
   
+
+  int denominador1;
+  cout << "Entre el denominador de su primera fraccion: " << endl;
+  cin >> denominador1;
+
+
+  int numerador2;
+  cout << "Entre el numerador de su segunda fraccion: " << endl;
+  cin >> numerador2;
+  
+
+  int denominador2;
+  cout << "Entre el denominador de su segunda fraccion: " << endl;
+  cin >> denominador2;
+
+
+
+
+
+
   return 0;
 }
